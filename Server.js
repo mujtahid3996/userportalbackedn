a8b12480bcb9f1db3a313a4d8a81102c41871402
mujtahid3996 <mujtahid2018@gmail.com>
2019-11-14 01:56:59 +0600

    changepassword now works and can see the user profile

--- Server.js
+++ Server.js
@@ -41,7 +41,7 @@ app.post('/Signin',(req,res) => {
 
     })
 })
-app.post('/Register',(req,res) =>{
+app.post('/Register',(req,res) => {
     const { firstname, lastname,email,address,phone,date,password} =req.body;
     const hash = bcrypt.hashSync(password);
     database('users').
@@ -60,6 +60,27 @@ app.post('/Register',(req,res) =>{
         res.status(200).json(user[0]);
     })  
     .catch(err => res.status(400).json(err))
+})
+app.post('/Changepassword',(req,res) => {
+    console.log(req.body.oldpassword);
+    database.select('email','password').from('users')
+    .where('email','=',req.body.email)
+    .then(data => {
+        const  issvalid = bcrypt.compareSync(req.body.oldpassword,data[0].password);
+        if(issvalid)
+        { 
+            return database('users').update('password',bcrypt.hashSync(req.body.newpassword))
+            .then(user =>{
+                
+                res.status(200).json('Changedsuccessfully')
+            })
+            .catch( err => res.status(400).json('unable to Changepassword'))   
+        }
+        else
+        {
+            res.status(400).json('wrong credential')
+        }
+    })
 })
 
 app.listen(3000);
