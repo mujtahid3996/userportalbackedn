09c49744ed40738991eca68fb21817779df2454a
mujtahid3996 <mujtahid2018@gmail.com>
2019-11-17 20:52:24 +0600

    i am fedup

--- Server.js
+++ Server.js
@@ -3,39 +3,23 @@ const bodyparser = require('body-parser');
 const knex = require('knex')
 var API = require('email-address-validation');
 const cors = require('cors');
-const app = express();
 const bcrypt = require('bcryptjs');
 const register =require('./Register/Register')
 const changepassword =require('./Changepassword/Changpassword')
 const signin = require('./Signin/Signin')
 const adminlogin=require('./AdminLogin/Adminlogin')
 const getusers=require('./Getusers/getusers')
-var allowedOrigins = ['http://localhost:3000',
-                      'https://mujtahids-user-portal.herokuapp.com'];
-
-app.use(bodyparser.json());
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
-app.options('*',cors());
 const database = knex ({
-    client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: true,
-     
-    }
-  });
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+   
+  }
+});
+const app = express();                      
+app.use(cors());
+app.use(bodyparser.json());
   var api = new API({
     access_key: '7e059c1a00cb82b6db65c827f2495f08',
   });
