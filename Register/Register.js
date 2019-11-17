e044f088a82027d0136ca898a48c70761bddc124
mujtahid3996 <mujtahid2018@gmail.com>
2019-11-17 13:38:15 +0600

    fixing

--- Register/Register.js
+++ Register/Register.js
@@ -1,4 +1,4 @@
-var  isemailvalid = 0;
+
 const handleRegister = (req,res,database,bcrypt,api) => {
     const { firstname, lastname,email,address,phone,date,password} =req.body;
     if(!firstname || !lastname || !email || !address ||!phone ||!password||!date)
@@ -9,36 +9,37 @@ const handleRegister = (req,res,database,bcrypt,api) => {
         api.check(checkQuery)
     .then(function (result) {
         isemailvalid = 1;
-        console.log('Check Promise Resolve' + JSON.stringify(result)+isemailvalid);
+        console.log('Check Promise Resolve' + JSON.stringify(result)+result.mx_found);
+        if(Boolean(isemailvalid)) {
+            const hash = bcrypt.hashSync(password);
+        database('users').
+        returning('*').
+        insert({
+           email: email,
+           firstname:firstname,
+           lastname:lastname,
+           address:address,
+           phone:phone,
+           password:hash,
+           birthday:date
+         
+        }).then(user => {
+            console.log(user[0])
+            res.status(200).json(user[0]);
+        })  
+        .catch(err => res.status(400).json(err))
+        }
+        else
+            {
+                res.status(400).json('wrong email entered');
+        }
     })
     .catch(function (err) {
-        isemailvalid = 0;
         console.log('Check Promise Reject: ' + JSON.stringify(err) +isemailvalid);
+        res.status(400).json('wrong email entered');
+
     });
-    console.log("email" +isemailvalid);
-    if(Boolean(isemailvalid)) {
-        const hash = bcrypt.hashSync(password);
-    database('users').
-    returning('*').
-    insert({
-       email: email,
-       firstname:firstname,
-       lastname:lastname,
-       address:address,
-       phone:phone,
-       password:hash,
-       birthday:date
-     
-    }).then(user => {
-        console.log(user[0])
-        res.status(200).json(user[0]);
-    })  
-    .catch(err => res.status(400).json(err))
-    }
-    else
-        {
-            res.status(400).json('wrong email entered');
-    }
+    
 }
 
 module.exports = {
