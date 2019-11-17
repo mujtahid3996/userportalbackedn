635359fc227c0d31551b6412aed7ca07cbd0dbc7
mujtahid3996 <mujtahid2018@gmail.com>
2019-11-17 13:51:01 +0600

    fixed hopefully

--- Register/Register.js
+++ Register/Register.js
@@ -1,45 +1,43 @@
 
-const handleRegister = (req,res,database,bcrypt,api) => {
-    const { firstname, lastname,email,address,phone,date,password} =req.body;
-    if(!firstname || !lastname || !email || !address ||!phone ||!password||!date)
+const handleRegister = (req, res, database, bcrypt, api) => {
+    const { firstname, lastname, email, address, phone, date, password } = req.body;
+    if (!firstname || !lastname || !email || !address || !phone || !password || !date)
         return res.status(400).json('incorrectform form submission');
-        var checkQuery = {
-            email: email
-        };
-        api.check(checkQuery)
-    .then(function (result) {
-        isemailvalid = 1;
-        console.log('Check Promise Resolve' + JSON.stringify(result)+result.mx_found);
-        if(Boolean(isemailvalid)) {
-            const hash = bcrypt.hashSync(password);
-        database('users').
-        returning('*').
-        insert({
-           email: email,
-           firstname:firstname,
-           lastname:lastname,
-           address:address,
-           phone:phone,
-           password:hash,
-           birthday:date
-         
-        }).then(user => {
-            console.log(user[0])
-            res.status(200).json(user[0]);
-        })  
-        .catch(err => res.status(400).json(err))
-        }
-        else
-            {
-                res.status(400).json('wrong email entered');
-        }
-    })
-    .catch(function (err) {
-        console.log('Check Promise Reject: ' + JSON.stringify(err) +isemailvalid);
-        res.status(400).json('wrong email entered');
+    var checkQuery = {
+        email: email
+    };
+    api.check(checkQuery)
+        .then(function (result) {
+            console.log('Check Promise Resolve' + JSON.stringify(result) + result.mx_found);
+            if (result.mx_found === 'true' && result.smtp_check === 'true') {
+                const hash = bcrypt.hashSync(password);
+                database('users').
+                    returning('*').
+                    insert({
+                        email: email,
+                        firstname: firstname,
+                        lastname: lastname,
+                        address: address,
+                        phone: phone,
+                        password: hash,
+                        birthday: date
 
-    });
-    
+                    }).then(user => {
+                        console.log(user[0])
+                        res.status(200).json(user[0]);
+                    })
+                    .catch(err => res.status(400).json(err))
+            }
+            else {
+                res.status(400).json('wrong email entered');
+            }
+        })
+        .catch(function (err) {
+            console.log('Check Promise Reject: ' + JSON.stringify(err) + isemailvalid);
+            res.status(400).json('wrong email entered');
+
+        });
+
 }
 
 module.exports = {
