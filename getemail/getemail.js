2bddfde7626fec9c9f491d226058e2db7c9937e7
mujtahid3996 <mujtahid2018@gmail.com>
2019-11-19 15:23:21 +0600

    a bit changed

--- getemail/getemail.js
+++ getemail/getemail.js
@@ -1,16 +1,20 @@
-const getemail = (req,res,database) =>{
-    const {email} =req.body;
+const getemail = (req, res, database) => {
+    const { email } = req.body;
     database.select('email').from('users')
-    .where('email','=',email)
-    .then(data => {
-        console.log(data)
-        res.status(400).json('email found' )}
+        .where('email', '=', email)
+        .then(data => {
+            if (data.email !== '')
+                res.status(400).json('email found')
+            else {
+                res.status(200).json('emailavailable')
+            }
+        }
         )
-    .catch(err => {
-        console.log(err)
-        res.status(200).json('emailavailable')
+        .catch(err => {
+            console.log(err)
+            res.status(200).json('emailavailable')
         })
 }
 module.exports = {
-    getemail :getemail
+    getemail: getemail
 }
