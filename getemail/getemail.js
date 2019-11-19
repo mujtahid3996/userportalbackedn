3dc56c582afd14cccfc2e53b296890f2b44630bc
mujtahid3996 <mujtahid2018@gmail.com>
2019-11-19 11:31:40 +0600

    checking

--- getemail/getemail.js
+++ getemail/getemail.js
@@ -2,8 +2,14 @@ const getemail = (req,res,database) =>{
     const {email} =req.body;
     database.select('email').from('users')
     .where('email','=',email)
-    .then(data => res.status(400).json('email found' ))
-    .catch(err => res.status(200).json('emailavailable'))
+    .then(data => {
+        console.log(data)
+        res.status(400).json('email found' )}
+        )
+    .catch(err => {
+        console.log(err)
+        res.status(200).json('emailavailable')
+        })
 }
 module.exports = {
     getemail :getemail
