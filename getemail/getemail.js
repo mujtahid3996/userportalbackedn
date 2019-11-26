d5e625dbe023ed6d2e3afd0ca95b3db0b0115a70
mujtahid3996 <mujtahid2018@gmail.com>
2019-11-26 10:33:15 +0600

    fixed a minor error;

--- getemail/getemail.js
+++ getemail/getemail.js
@@ -1,18 +1,38 @@
-const getemail = (req, res, database) => {
+const getemail = (req, res, database, api) => {
     const { email } = req.body;
-    database.select('email').from('users')
-        .where('email', '=', email)
-        .then(data => {
-            if (data[0])
-                res.status(200).json('email found')
-            else {
-                res.status(200).json('emailavailable')
+    if (!email) {
+        res.status(400).json('blankemail');
+    }
+    var checkQuery = {
+        email: email
+    };
+    api.check(checkQuery)
+        .then(function (result) {
+            console.log('Check Promise Resolve' + JSON.stringify(result) + result.mx_found);
+            if (result.mx_found === true && result.smtp_check === true && result.format_valid === true) {
+                database.select('email').from('users')
+                    .where('email', '=', email)
+                    .then(data => {
+                        if (data[0])
+                            res.status(200).json('email found')
+                        else {
+                            res.status(200).json('emailavailable')
+                        }
+                    }
+                    )
+                    .catch(err => {
+                        console.log(err)
+                        res.status(200).json('emailavailable')
+                    })
             }
-        }
-        )
-        .catch(err => {
-            console.log(err)
-            res.status(200).json('emailavailable')
+            else {
+                res.status(400).json('unavailableemail')
+            }
+        })
+        .catch(function (err) {
+            console.log('Check Promise Reject: ' + JSON.stringify(err) + isemailvalid);
+            res.status(400).json('wrong email entered');
+
         })
 }
 module.exports = {
